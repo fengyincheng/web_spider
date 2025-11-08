4a8cda19ee85227eee1d6d058908cb28440a4f05
fengyincheng <3035714895@qq.com>
2025-11-08 20:43:27 +0800

    成功爬取

--- scrape_douban.py
+++ scrape_douban.py
@@ -1,10 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
 }
-respond = requests.get("https://movie.douban.com/top250",headers= headers)
-if respond.ok:
-    print("拿下!")
-else:
-    print("fail......")
+for start_num in range(0,250,25):
+    print(start_num)
 
+    respond = requests.get(f"https://movie.douban.com/top250?start={start_num}",headers= headers)
+    # if respond.ok:
+    #     print("拿下!")
+    # else:
+    #     print("fail......")
+
+    html = respond.text
+    beautifulSoup = BeautifulSoup(html,"html.parser")
+    all_title = beautifulSoup.find_all("span",attrs = {"class":"title"})
+    for title in all_title:
+        title_string = title.string
+        if "/" not in title_string:
+            print(title_string)
